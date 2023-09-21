cf54b8dca9b55f65552e1d45a7b1e27187827c41
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-09-21 13:21:24 +0100

    now connecting to the database

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,32 +1,28 @@
-import express from 'express'
 require('dotenv').config()
-const {graphqlHTTP} = require('express-graphql')
+import express from 'express'
+// import pool from './db.ts'
+const pool = require('./db')
 
+// set up graphQL server
+const {graphqlHTTP} = require('express-graphql')
 
 //* creates an express app
 const port = process.env.PORT || 4000
 const app = express();
 app.use(express.json());
 
-//* import {connectToDb, getDb} from './db'
-const {ObjectId} = require('mongodb');
-const {connectToDb, getDb} = require('./db')
-
-
-//* opens connection to the mongodb database before listening for request
-let db: any
-connectToDb((err: any) => {
-    if (!err) {
-        // now we can start listening for events
-        app.listen(port, () => {
-            console.log(`now listening to request from port ${port}`)
-        })
-
-        // updates our database variable
-        db = getDb()
-    } else {
-        console.log(`we have an error, error: ${err}`)
+// connect to the database and then allow express to receive request
+pool.connect((err: any, client: any, release: () => void) => {
+    if (err) {
+        return console.log('Error connecting to the postgresSQL database, because: ', err.stack)
     }
+
+    app.listen(port, () => {
+        console.log(`now listening to request from port ${port}`)
+        // routes(app)
+    })
+
+    release()
 })
 
 
