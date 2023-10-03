c60e15b3b78d05395c8d138cf63bcbc9358444f6
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-10-03 08:37:38 +0100

    checked to see that there are no existing usernames or emails

--- server/src/App.ts
+++ server/src/App.ts
@@ -23,23 +23,25 @@ const port = process.env.PORT || 4000
 const app = express();
 app.use(express.json());
 
-// connect to the database and then allow express to receive request
-app.listen(port, () => {
-    console.log(`now listening to request from port ${port}`)
-    routes(app)
-})
-// pool.connect((err: any, client: any, release: () => void) => {
-//     if (err) {
-//         return console.log('Error connecting to the postgresSQL database, because: ', err.stack)
-//     }
-
-//     app.listen(port, () => {
-//         console.log(`now listening to request from port ${port}`)
-//         routes(app)
-//     })
-
-//     release()
+// for quick opening of port if you don't have time to wait for the database connection before opening of port
+// app.listen(port, () => {
+//     console.log(`now listening to request from port ${port}`)
+//     routes(app)
 // })
+
+// connect to the database and then allow express to receive request
+pool.connect((err: any, client: any, release: () => void) => {
+    if (err) {
+        return console.log('Error connecting to the postgresSQL database, because: ', err.stack)
+    }
+
+    app.listen(port, () => {
+        console.log(`now listening to request from port ${port}`)
+        routes(app)
+    })
+
+    release()
+})
 
 
 // app.listen(port, () => {
--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -1,5 +1,6 @@
 import * as bcrypt from 'bcrypt';
-import { log } from '../logger/';
+import { errorLogger, log } from '../logger/';
+import pool from '../db'
 
 import {userRegisterInfo} from '../types/users'
 import {show_bad_message, show_good_message} from '../functions/utils'
@@ -8,6 +9,7 @@ export async function register_a_new_user(userInfo:userRegisterInfo) {
     const {name, username, email, password, gender} = userInfo;
     const checks_array = [name, username, email, password, gender] // want to make sure all fields are not less than zero
     let found_an_empty_value = false
+    const success_message = 'Your registration was successfully completed, You can now login with your username/email and password'
 
     // checks to make sure all fields are not less than zero in length
     checks_array.forEach(item => {
@@ -16,6 +18,17 @@ export async function register_a_new_user(userInfo:userRegisterInfo) {
         } 
     })
  
+    // checks to see if the email already exists
+    const qEmail = await pool.query("SELECT id from users where email = $1 limit 1", [email])
+    if (qEmail.rows.length > 0) {
+        return show_bad_message('This email already exists')
+    }
+
+    // checks to see if the username already exists
+    const qUsername = await pool.query("SELECT id from users where username = $1 limit 1", [username])
+    if (qUsername.rows.length > 0) {
+        return show_bad_message('This username already exists')
+    }
 
     if (found_an_empty_value) {
         return show_bad_message('please fill up all the required fields, some fields are empty')
@@ -25,9 +38,22 @@ export async function register_a_new_user(userInfo:userRegisterInfo) {
         return show_bad_message('Your password is too short, it should be at least 5 characters')
     }
 
-    const plainPassword: string = 'user_password'; // Replace with the actual password
-    const saltRounds: number = 10; // Number of salt rounds, higher is better but slower
-    // console.log('oya, register the user shaparly', userInfo)
+    // hash the password using bcrypt
+    const saltWorkFactor = 10;
+    const salt = await bcrypt.genSalt(saltWorkFactor);
+    const hash = await bcrypt.hashSync(password, salt);
 
-    return show_good_message()
+    // saves the new user to the database
+    try {
+        const newTodo = await pool.query(
+            "INSERT INTO users (name, username, email, password, gender, date_added) VALUES ($1, $2, $3, $4, $5, now()) RETURNING *",
+            [name.toLowerCase(), username.toLowerCase(), email.toLowerCase(), hash, gender]
+        )
+        return show_good_message(success_message)
+    } catch (err:any) {
+        errorLogger.error({pre:'Could not save a new user to the database'}, err.message);
+        return show_bad_message('Could not save your information to the database, please contact our customer support for assistance');
+    }
+
+    return show_good_message(success_message)
 }
