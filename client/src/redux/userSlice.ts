401b4dc287d87b26ea172fdc043d3b65355b6a5a
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-10-10 17:13:31 +0100

    almost done with setting up the Redux store and Typescript set-ups

--- client/src/redux/userSlice.ts
+++ client/src/redux/userSlice.ts
@@ -1,11 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+// Define the initial state
+type userDetailsType = {
+    name: string;
+    userId:number;
+    session_fid:number
+}
+const initialState: userDetailsType = { name: '', userId:0, session_fid:0};
+
+// Create a slice
 const userSlice = createSlice({
     name: 'user',
-    initialState: {
-        name: '',
-        email:'',
-    }
-})
+    initialState,
+    reducers: {
+        // Reducer to set the count to a specific value
+        updateUser: (state, action: PayloadAction<userDetailsType>) => {
+            return state = {...state, ...action.payload};
+        },
+    },
+});
 
-export default userSlice.reducer
+// Export the actions and reducer
+export const { updateUser } = userSlice.actions;
+export default userSlice.reducer;
