98e46f1d0294bf10099fa079e8e65a94b8a00619
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-10-13 10:31:03 +0100

    done with left side design

--- client/src/components/Profile/ProfileComp.tsx
+++ client/src/components/Profile/ProfileComp.tsx
@@ -2,13 +2,14 @@ import {BsFillCheckCircleFill, BsTrash} from 'react-icons/bs'
 import {FaPencilAlt} from 'react-icons/fa'
 
 import Header from "../Header/Header";
+import './Profile.scss'
 
 export default function ProfileComp() {
     return (
         <div className="profile_Cvr1">
             <Header />
             <div className="padding-x flex">
-                <div className="">
+                <div className="w-[60%]">
                     <div className="my-10">
                         <div className="text-2xl"><h2>Add items to your todo list</h2></div>
                         <div className="py-4 flex">
@@ -22,18 +23,52 @@ export default function ProfileComp() {
                         </div>
                     </div>
                     <div className="">
-                        <div className=""><h2>Get busy</h2></div>
+                        <div className="p-6 bg-[#f1f2f6] font-semibold"><h2>Get busy</h2></div>
                         <div className="">
-                            <div className="flex">
-                                <div className="relative">
-                                    <div className=""><FaPencilAlt /></div>
-                                    <div className=""><BsTrash /></div>
+                            <div className="todoEch flex py-5">
+                                <div className="flex space-x-3 items-center mr-4 w-[80px]">
+                                    <div className="icons"><FaPencilAlt /></div>
+                                    <div className="icons"><BsTrash /></div>
+                                </div>
+                                <div className="">
+                                    <div className="">Take out the trash and call Mr ade to come and fix all the fans down stairs tomorrow morning, Take out the trash and call Mr ade to come and fix all the fans down stairs tomorrow morning</div>
+                                    <div className="text-xs font-semibold text-[#a4b0be] mt-2">Added 20mins ago</div>
+                                </div>
+                            </div>
+
+                            <div className="todoEch flex py-5">
+                                <div className="flex space-x-3 items-center mr-4 w-[80px]">
+                                    <div className="icons"><FaPencilAlt /></div>
+                                    <div className="icons"><BsTrash /></div>
                                 </div>
                                 <div className="">
                                     <div className="">Take out the trash and call Mr ade to come and fix all the fans down stairs tomorrow morning</div>
-                                    <div className="">added 20mins ago</div>
+                                    <div className="text-xs font-semibold text-[#a4b0be] mt-2">Added 20mins ago</div>
                                 </div>
                             </div>
+
+                            <div className="todoEch flex py-5">
+                                <div className="flex space-x-3 items-center mr-4 w-[80px]">
+                                    <div className="icons"><FaPencilAlt /></div>
+                                    <div className="icons"><BsTrash /></div>
+                                </div>
+                                <div className="">
+                                    <div className="">Take out the trash and call Mr ade to come and fix all the fans down stairs tomorrow morning</div>
+                                    <div className="text-xs font-semibold text-[#a4b0be] mt-2">Added 20mins ago</div>
+                                </div>
+                            </div>
+
+                            <div className="todoEch flex py-5">
+                                <div className="flex space-x-3 items-center mr-4 w-[80px]">
+                                    <div className="icons"><FaPencilAlt /></div>
+                                    <div className="icons"><BsTrash /></div>
+                                </div>
+                                <div className="">
+                                    <div className="">Take out the trash and call Mr ade to come and fix all the fans down stairs tomorrow morning</div>
+                                    <div className="text-xs font-semibold text-[#a4b0be] mt-2">Added 20mins ago</div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
